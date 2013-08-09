af38adf2fbee2b637737ffd80e1d5f867e22f9b5
Yap Cheah Shen <yapcheahshen@gmail.com>
2013-08-09 23:05:00 +0800

    remove unneed main.js , now application code is 100% cross platform.

--- node_webkit/auraext/aura-yadb.js
+++ node_webkit/auraext/aura-yadb.js
@@ -1,22 +1,26 @@
 /*
-  aura-ydb interface for node-webkit.
-  this file serve like the role of socket.io client side rpc interface
+  aura-ydb interface for node-webkit and node server.
+  bridge node context and DOM context only in aura-extensions
+  do not call node function directly in widgets js
 */
 define(function() {
   return {
  
  initialize: function(app) {
      if (typeof process !='undefined' &&process.versions['node-webkit']) {
+        /* compatible async interface for browser side js code*/
         var api_yadb=require('yadb').api ; 
         if (!app.sandbox.services) app.sandbox.services={};
-        api_yadb(app.sandbox.services);
-          app.sandbox.yadb={ //turn into async, for compatible with socket.io
+        api_yadb(app.sandbox.services); //install api into services
+          app.sandbox.yadb={ //turn into async, for compatible with node_server
             getRaw: function( path, callback) {
               var data=app.sandbox.services["yadb"].getRaw(path);
-            setTimeout( function() { callback(0,data) }, 0);
+              //this line is not really needed.
+              setTimeout( function() { callback(0,data) }, 0);
           }
         };  
      } else {
+        //for node_server , use socket.io to talk to server-side yadb_api.js
         requirejs(['../node_server/cli-js/rpc_yadb'],function(yadb){
           app.sandbox.yadb=yadb;
         });
