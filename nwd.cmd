df976738a74f53fb841bbad61c4b316f1133dce4
Yap Cheah Shen <yapcheahshen@gmail.com>
2013-08-20 16:47:47 +0800

    remove version dependency of node-webkit

--- nwd.cmd
+++ nwd.cmd
@@ -1,2 +1,2 @@
 @start http://127.0.0.1:9222/
-@start node_webkit\node-webkit-v0.6.3-win-ia32\nw.exe --remote-debugging-port=9222 %1
+@start node_webkit\win-ia32\nw.exe --remote-debugging-port=9222 %1
--- ydbexplorer.cmd
+++ ydbexplorer.cmd
@@ -1,2 +1,2 @@
 start http://127.0.0.1:9222/
-start node_webkit\node-webkit-v0.6.3-win-ia32\nw --remote-debugging-port=9222 ydbexplorer
+start node_webkit\win-ia32\nw --remote-debugging-port=9222 ydbexplorer
