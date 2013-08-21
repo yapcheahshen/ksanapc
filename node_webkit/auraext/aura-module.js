5752278cb19631f4f8594b8d534322f6b0d2a69c
Yap Cheah Shen <yapcheahshen@gmail.com>
2013-08-21 22:46:42 +0800

    fix module version check 0.0.10 less than 0.0.9

--- node_webkit/auraext/aura-module.js
+++ node_webkit/auraext/aura-module.js
@@ -2,9 +2,10 @@ define({
   initialize: function(app) {
 	app.sandbox.minversion=function(mod, ver) {
 		var m=app.sandbox[mod].version.match(/(\d*?)\.(\d*?)\.(\d*)/);
+		console.log('module version',mod,m)
 		var v=ver.match(/(\d*?)\.(\d*?)\.(\d*)/);
 		if (!m) return false;
-		return  (m[1]>=v[1] && m[2]>=v[2] && m[3]>=v[3]);
+		return  (parseInt(m[1])>=parseInt(v[1]) && parseInt(m[2])>=parseInt(v[2]) && parseInt(m[3])>=parseInt(v[3]));
 	}
   },
   afterAppStart: function() {
