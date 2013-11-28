f214bb14b196dbafad7361abae9c2a5d8982ae68
yapcheahshen <yapcheahshen@gmail.com>
2013-11-29 01:26:50 +0800

    create _children for each instance

--- node_webkit/auraext/nestedview.js
+++ node_webkit/auraext/nestedview.js
@@ -2,15 +2,16 @@
 /* backbone nested view extension 
 inspired from 
 http://blog.shinetech.com/2012/10/10/efficient-stateful-views-with-backbone-js-part-1/
-http://bardevblog.wordpress.com/2012/12/13/re-learning-backbone-js-nested-views/
+http://bardevblog.wordpress.com/2012/12/13/re-learning-backbone-js-nested-views
+http://stackoverflow.com/questions/9337927/how-to-handle-initializing-and-rendering-subviews-in-backbone-js
+http://www.sophomoredev.com/2012/06/a-different-approach-to-rendering-backbone-sub-views/
 */
 define(['jquery','underscore','backbone'],function($,_,Backbone){
 	var nestedView=Backbone.View.extend({
       $yase: function(){ //promise interface
             return this.sandbox.$yase.apply(this,arguments)
       },
-      _children:[],
-      init:function() { //maybe removed if aura provide a hook..
+      initNested:function() { //maybe removed if aura provide a hook..
       	this.$el.data('hview',this);
       },
       closeChildren:function() {
@@ -19,22 +20,19 @@ define(['jquery','underscore','backbone'],function($,_,Backbone){
                   if (C.destroy) C.close(); else (C.remove());
             }
       },
-      addChildren:function(classname) {
-      	//automatic add all child view, call this after html()
-      	classname=classname||'data-aura-widget'
-      	var children=this.$("div."+classname);
-      	this.closechildren();
+      addChildren:function(attrname) { //automatic add all child view, call this after html()
+      	attrname=attrname||'data-aura-widget'
+      	var children=this.$("div["+attrname+"]");
+      	this.closeChildren();
+            this.children=[];
       	for (var i=0;i<children.length;i++) {
-      		var hview=children[i].data(hview);
-      		this.addchild(hview);
+      		this.addChild($(children[i]));
       	}
       },
       newAuraComponent:function(componentName,opts){
             if (!componentName) return;
             var attributes='"';
-            for (var i in opts) {
-                  attributes+=' '+i+'="'+opts[i]+'"';
-            }
+            for (var i in opts) attributes+=' '+i+'="'+opts[i]+'"';
             var $child=$('<div><div data-aura-widget="'+componentName+
                   attributes+'></div></div>');
             this.$("#children").append($child);
@@ -42,6 +40,7 @@ define(['jquery','underscore','backbone'],function($,_,Backbone){
             this.addChild($child);
       },      
       _addChild:function(child) {
+
             //TODO check if pure dom object
             if (child instanceof $) {
                   var hview=child.data('hview');
@@ -50,6 +49,10 @@ define(['jquery','underscore','backbone'],function($,_,Backbone){
                   }
                   this.addChild(hview);
             } else {
+                  if (!child) {
+                        console.error('adding empty child');
+                        return;
+                  }
                   child._parent=this;
                   if (_(this._children).indexOf(child) === -1) {
                         this._children.push(child);
@@ -58,7 +61,8 @@ define(['jquery','underscore','backbone'],function($,_,Backbone){
             return child;
       },
       addChild: function(child) {
-      	if (!child)return;      	
+      	if (!child)return;
+            if (!this._children) this._children=[];
             if (!this._children.length) {
                   //work around for first child is not initialized immediately
                   setTimeout(this._addChild.bind(this,child),200);      
@@ -75,38 +79,64 @@ define(['jquery','underscore','backbone'],function($,_,Backbone){
       	if (this._parent) this._parent.removeChild(this);
       	this.remove();
       },
-      receive:{},
+      commands:{},
       //send command to all children
-      send:function() {
+      sendChildren:function() {
       	for (var i in this._children) {
       		var C=this._children[i];
-      		if (typeof C.receive=='function'){
-      			C.receive(arguments);
-      		} else { // events-like comand map
-      			var func=C.receive[arguments[0]];
-      			var remain=Array.prototype.slice.apply(arguments,[1]);
-      			if (func) func.apply(C,remain);
-      			//else console.warn('cannot send '+arguments[0]);
-      		}
+			var func=C.commands[arguments[0]];
+			var remain=Array.prototype.slice.apply(arguments,[1]);
+                  if (typeof func==="string") {
+                        var f=C[func];
+                        if (f) func=f;
+                        else {
+                              console.error('function '+func+' not found');
+                              return;
+                        }
+                  }
+			if (func) func.apply(C,remain);
+			//else console.warn('cannot send '+arguments[0]);
       	}
       	return this;
+      },
+      sendParent:function() { //send to parent
+            if (!this._parent) {
+                  console.error('no parent');
+                  return;
+            }
+            var func=this._parent.commands[arguments[0]];
+            var remain=Array.prototype.slice.apply(arguments,[1]);
+            if (typeof func==="string") {
+                  var f=this._parent[func];
+                  if (f) func=f;
+                  else {
+                        console.error('function '+func+' not found');
+                        return;
+                  }
+            }
+            if (func) func.apply(this._parent,remain);
+            
       },
       sendAll:function() { //send to all descendant
             for (var i in this._children) {
                   var C=this._children[i];
-                  if (typeof C.receive=='function'){
-                        C.receive(arguments);
-                  } else { // events-like comand map
-                        var func=C.receive[arguments[0]];
-                        var remain=Array.prototype.slice.apply(arguments,[1]);
-                        if (func) func.apply(C,remain);
-                        //else console.warn('cannot send '+arguments[0]);
+                  var func=C.commands[arguments[0]];
+                  var remain=Array.prototype.slice.apply(arguments,[1]);
+                  if (typeof func==="string") {
+                        var f=C[func];
+                        if (f) func=f;
+                        else {
+                              console.error('function '+func+' not found');
+                              return;
+                        }
                   }
+
+                  if (func) func.apply(C,remain);
+                  //else console.warn('cannot send '+arguments[0]);
                   C.send.apply(C,arguments);
             }
             return this;            
       }
-
     });
     return nestedView;
 });
